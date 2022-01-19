ce493c4078a8495d7b1cd9d707ee2f4ced14efd6
DannyGlade <DannyGlade@dg.com>
2022-01-19 20:28:23 +0530

    added hash password security

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -54,7 +55,8 @@ class UserController extends Controller
         $user = new User;
         $user->name = $request->name;
         $user->email = $request->email;
-        $user->password = md5($request->password);
+        $user->password = Hash::make($request->password);
+        // $user->password = md5($request->password);
         $user->save();
 
         return redirect(url('/login'));
