c701a70e6722ada91bf5bf5e9310b14301cc169c
DannyGlade <DannyGlade@dg.com>
2022-01-24 08:21:55 +0530

    lil tweaks

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -101,9 +101,29 @@
                     </div>
                 @endforeach
             </div>
+
             <hr class="featurette-divider">
 
             <div class="row featurette">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your
+                            mind.</span></h2>
+                    <p class="lead">Some great placeholder content for the first featurette here. Imagine some
+                        exciting prose here.</p>
+                </div>
+                <div class="col-md-5">
+                    <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
+                        height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500"
+                        preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa"
+                            dy=".3em">500x500</text>
+                    </svg>
+
+                </div>
+            </div>
+
+            {{-- <div class="row featurette">
                 <div class="col-md-7">
                     <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your
                             mind.</span></h2>
@@ -163,6 +183,8 @@
 
                 </div>
             </div>
+
+            <hr class="featurette-divider"> --}}
 
             <hr class="featurette-divider">
 
