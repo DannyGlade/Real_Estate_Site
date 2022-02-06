7c562fddb5c3088a7b37130ad0e2c0c4442a2a26
DannyGlade <DannyGlade@dg.com>
2022-02-06 14:56:12 +0530

    routes, migrations, default img

---
+++ database/migrations/2022_02_06_062102_create_user_data_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_data', function (Blueprint $table) {
+            $table->id();
+            $table->string('image')->nullable();
+            $table->string('about')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_data');
+    }
+}
